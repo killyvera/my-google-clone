ba575b554656f9f67fda808511852922b52f9271
killyvera <david.esmeralda@gmail.com>
2022-08-15 14:16:05 -0500

    add iconst to links array

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const links = [
+    { url: '/search', text: '🔎 All' },
+    { url: '/news', text: '📰 News' },
+    { url: '/images', text: '📷 Images' },
+    { url: '/videos', text: '📺 Videos' },
+];
 
 export const Links = () => {
-  return (
-    <div>Links</div>
-  )
-}
+    return (
+        <div className='flex sm:justify-around justify-between items-center mt-4'>
+            {links.map(({ url, text }) => (
+                <NavLink to={url} activeClassName='text-blue-700 border-b-2 dark:text-blue-300 boder-blue-700 p-2'>
+                {text}
+            </NavLink>
+            ))}
+        </div>
+    )
+};
