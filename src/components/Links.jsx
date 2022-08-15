12d58d1255eb44b44ff242222e238c06af7c7e70
killyvera <david.esmeralda@gmail.com>
2022-08-15 14:37:57 -0500

    solved navlink new version active classes conf

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -12,7 +12,7 @@ export const Links = () => {
     return (
         <div className='flex sm:justify-around justify-between items-center mt-4'>
             {links.map(({ url, text }) => (
-                <NavLink to={url} activeClassName='text-blue-700 border-b-2 dark:text-blue-300 boder-blue-700 p-2'>
+                <NavLink to={url} className={({ isActive }) => isActive? "text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 p-2": 'm-2'}>
                 {text}
             </NavLink>
             ))}
